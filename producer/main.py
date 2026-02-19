3dcc7dfd7d1334b03b9513f39b717d8df1ca9d8c
MEIRG <meirg2001@gmail.com>
2026-02-19 11:29:58 +0200

    fix: change the env variable because it's not working for some reason

--- producer/main.py
+++ producer/main.py
@@ -1,10 +1,14 @@
 import json
 import os 
 
-PATH = os.getenv(PATH, r'data\border_alerts.json') # type: ignore
+# PATH = os.getenv('PATH', r'data\border_alerts.json') 
+PATH = r'data\border_alerts.json' 
 class ReadData:
     def get_notification_data(self, path):
         with open(path) as file:
             data = json.load(file)
             return data
 
+data = ReadData() 
+notification = data.get_notification_data(PATH)
+print(notification)
