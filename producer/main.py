e4d04208f9ebbf3741ddb9459f48099d5547a375
MEIRG <meirg2001@gmail.com>
2026-02-19 11:25:43 +0200

    add env variable to json file

--- producer/main.py
+++ producer/main.py
@@ -1,6 +1,7 @@
 import json
+import os 
 
-
+PATH = os.getenv(PATH, r'data\border_alerts.json') # type: ignore
 class ReadData:
     def get_notification_data(self, path):
         with open(path) as file:
