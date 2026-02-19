2891fa1a09be318e686fc9900c2af6281e6e851c
MEIRG <meirg2001@gmail.com>
2026-02-19 12:42:57 +0200

    add method for lopping through the notifications and determine whether it's urgent or nurmal

--- producer/priority_logic.py
+++ producer/priority_logic.py
@@ -34,38 +34,62 @@ class Priority:
     def __init__(self, notifications: List[Data]):
         self.notifications = notifications 
 
-    def check_if_weapons(self, notification: Data) -> bool:
+    def _check_if_weapons(self, notification: Data) -> bool:
         """checks if there's weapon in there """
         return notification.weapons_count > 0
     
-    def check_distance_from_fence_m(self, notification: Data) -> bool:
+    def _check_distance_from_fence_m(self, notification: Data) -> bool:
         """check if the distance is 50 meter less than"""
         return notification.distance_from_fence_m <= 50
     
-    def check_people_count(self, notification: Data) -> bool:
+    def _check_people_count(self, notification: Data) -> bool:
         """check if there is 8 poeple or less"""
         return notification.people_count >= 8
      
-    def check_vehicle_type(self, notification: Data) -> bool:
+    def _check_vehicle_type(self, notification: Data) -> bool:
         """check if the vehicle_type is truck"""
         return notification.vehicle_type == 'truck'
     
-    def check_distance_and_people(self, notification: Data) -> bool:
+    def _check_distance_and_people(self, notification: Data) -> bool:
         """check if the distance is 150 or less and people count is 4 or more poeple"""
         return notification.distance_from_fence_m <= 150 and notification.people_count >= 4 
         
-    def check_vehicle_and_people(self, notification: Data) -> bool:
+    def _check_vehicle_and_people(self, notification: Data) -> bool:
         """check if the vehicle_type is jeep and people count is 3 or more poeple"""
         return notification.vehicle_type == 'jeep' and notification.people_count >= 3 
     
     
-    def add_priority_field(self, priority, notification: Data):
+    def _add_priority_field(self,notification: Data, priority):
+        """add a priority field to a notification based on the priority it gets"""
         if priority == 'URGENT':
             notification.priority = 'URGENT'
         elif priority == 'NORMAL':
             notification.priority = 'NORMAL' 
 
-        
+    def check_notification(self):
+        """the main function for checking the notification priority type"""
+        urgent = 'URGENT'
+        normal = 'NORMAL'
+        for n in self.notifications:
+            if self._check_if_weapons(n):
+                self._add_priority_field(n, urgent)
 
+            elif self._check_distance_from_fence_m(n):
+                self._add_priority_field(n, urgent)
+
+            elif self._check_people_count(n):
+                self._add_priority_field(n, urgent)
+                
+            elif self._check_vehicle_type(n):
+                self._add_priority_field(n, urgent)
+
+            elif self._check_distance_and_people(n):
+                self._add_priority_field(n, urgent)
+
+            elif self._check_vehicle_and_people(n):
+                self._add_priority_field(n, urgent)
+            else: 
+                self._add_priority_field(n, normal)
+                
 
     
