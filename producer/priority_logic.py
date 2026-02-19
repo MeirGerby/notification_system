7646a2cc89bd5c76385143216bb149ffcf16d741
MEIRG <meirg2001@gmail.com>
2026-02-19 11:42:18 +0200

    docs: add class Data for json data

---
+++ producer/priority_logic.py
@@ -0,0 +1,13 @@
+class Data:
+    def __init__(self, border, zone, timestamp, people_count, weapons_count, vehicle_type, distance_from_fence_m, visibility_quality)
+        self.border  = border
+        self.zone  = zone
+        self.timestamp  = timestamp
+        self.people_count  = people_count
+        self.weapons_count  = weapons_count
+        self.vehicle_type  = vehicle_type
+        self.distance_from_fence_m  = distance_from_fence_m
+        self.visibility_quality  = visibility_quality
+
+
+# class Priority:
